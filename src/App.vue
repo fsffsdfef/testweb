3c7ff3e116882cfb8ae46324d2aa46b4cff4c918
sw.fan <sw.fan@trip.com>
2025-02-28 17:27:43 +0800

    commit message

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import index from './views/index.vue'
+</script>
+
+<template>
+  <div id="app">
+    <index></index>
+  </div>
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <el-container class="layout">
+    <el-aside class="aside" width="200px">Aside</el-aside>
+    <el-container>
+      <el-header class="header">Header</el-header>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style scoped>
+.layout {
+  height: 100%;
+}
+
+.aside {
+  background-color: antiquewhite;
+}
+
+.header {
+  background-color: aquamarine;
+}
+
+.main {
+  background-color: azure;
+}
+</style>
